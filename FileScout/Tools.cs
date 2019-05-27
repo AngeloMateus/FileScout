9d085692627b8a925a8f91500968ea66b3535144
Angelo.Mateus <34476066+AngeloMateus@users.noreply.github.com>
2019-05-27 18:15:53 +0100

    recursively delete files and folders in non-empty folder

---
+++ FileScout/Tools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileScout
+{
+    public static class Tools
+    {
+        public static bool IsEmpty<T>(this IEnumerable<T> items)
+        {
+            return items != null && !items.Any();
+        }
+    }
+}
