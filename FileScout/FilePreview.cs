15c337b5ca4bab1a37ac78a06ecf60e2e367e74d
Angelo.Mateus <34476066+AngeloMateus@users.noreply.github.com>
2019-09-12 19:53:27 +0100

    added filepreview file

---
+++ FileScout/FilePreview.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FileScout
+{
+    class FilePreview
+    {
+        public FilePreview()
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                Tools.DisplayError( e );
+            }
+        }
+    }
+}
